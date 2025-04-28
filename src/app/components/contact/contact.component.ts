c907752c505af945ad80ed5b91ae0277f948c11b
Ruizerick26 <ruierick2525@gmail.com>
2025-04-28 16:48:26 -0500

    modal dialog

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { AbstractType, Component, signal } from '@angular/core';
+import { AbstractType, ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -13,6 +13,10 @@ import {
   FormGroup,
   AbstractControl, } from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import {MatDialog, MatDialogModule, MatDialogTitle, MatDialogContent, MatDialogActions} from '@angular/material/dialog'
+
+
+
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -21,13 +25,18 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+
+
+
 @Component({
   selector: 'app-contact',
   imports: [MatDividerModule,MatFormFieldModule,MatButtonModule, FormsModule, MatInputModule, ReactiveFormsModule  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactComponent {
+  readonly dialog = inject(MatDialog);
   matcher = new MyErrorStateMatcher();
 
   contatForm = new FormGroup({
@@ -39,11 +48,21 @@ export class ContactComponent {
 
   onSubmit(){
     console.log("enviado")
+    console.log(this.contatForm)
     console.warn(this.contatForm.value)
     console.log('-------')
+    this.dialog.open(messagedialog)
     this.contatForm.reset()
     this.contatForm.controls.email.setErrors(null)
     this.contatForm.controls.name.setErrors(null)
     this.contatForm.controls.lastname.setErrors(null)
   }
-}
+} 
+
+@Component({
+  selector: 'dialogic-messsaje',
+  templateUrl: 'dialogic-message.html',
+  imports: [MatDialogModule, MatDialogActions, MatDialogContent, MatDialogTitle, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class messagedialog {}
