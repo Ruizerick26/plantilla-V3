e8d0cfa32a41f605adfa474c5a57d41b76139bf8
Ruizerick26 <ruierick2525@gmail.com>
2025-05-16 18:31:15 -0500

    correccion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,10 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+// import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch())]
 };
 
 
